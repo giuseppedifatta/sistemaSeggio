efe0c1afcc5b4ad8a5cfd88ab46a6b20c361aa2c
Giuseppe Di Fatta <giuseppe.difatta89@gmail.com>
2017-09-29 19:55:46 +0200

    aggiunto supporto ai token

---
+++ hardwaretoken.cpp
@@ -0,0 +1,6 @@
+#include "hardwaretoken.h"
+
+HardwareToken::HardwareToken()
+{
+
+}
---
+++ hardwaretoken.h
@@ -0,0 +1,11 @@
+#ifndef HARDWARETOKEN_H
+#define HARDWARETOKEN_H
+
+
+class HardwareToken
+{
+public:
+    HardwareToken();
+};
+
+#endif // HARDWARETOKEN_H
